6f82c4d554c8574e8eb6bde0ace9842b3642dad6
AntoJebi7 <dantojebi@gmail.com>
2024-09-01 20:31:23 +0530

    html integration

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,14 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    #return HttpResponse("Hello First Page")
+    return render(request,'index.html') 
 
 def detail(request,post_id):
-    return HttpResponse(f"post detail page , ID is {post_id}")
+    #return HttpResponse(f"post detail page , ID is {post_id}")
+    return render(request,'detail.html') 
+    
+    
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
