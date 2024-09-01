24c0e0c63a4852d62fe99e84f5faef2942983e69
AntoJebi7 <dantojebi@gmail.com>
2024-09-01 22:31:28 +0530

    Layout and Static

--- myapp/settings.py
+++ myapp/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    "blog/static/blog/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
